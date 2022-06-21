15a3caf9cf9dfcf1d2ec316f631359b46b007e2a
Hyan Lee <hyan23lee@hotmail.com>
2022-06-21 13:55:12 +0800

    wave animation

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use sdl2::render::{Canvas, RenderTarget};
 use std::time::Duration;
 
 struct canvas_buffer {
-    brightness: [[u8;800];600],
+    brightness: Vec<Vec<u8>>,
     phase: i32
 }
 
@@ -46,6 +46,34 @@ impl canvas_buffer {
             }
         }
     }
+    
+    pub fn circle1(&mut self, center: Point, radius: f32) {
+        for y in 0..600i32 {
+            for x in 0..800i32 {
+                let r = (((x - center.x).pow(2) + (y - center.y).pow(2)) as f32).sqrt();
+                self.brightness[y as usize][x as usize] = (r / 500f32 * 255f32) as u8;
+            }
+        }
+    }
+    
+    pub fn circle2(&mut self, center: Point, radius: f32) {
+        for y in 0..600i32 {
+            for x in 0..800i32 {
+                let r = (((x - center.x).pow(2) + (y - center.y).pow(2)) as f32).sqrt();
+                let b = 100f32 + ((r *(1f32-r/1400f32)/5f32 + self.phase as f32).sin() + 1f32) / 2f32 * 50f32;
+                self.brightness[y as usize][x as usize] =( b * (1f32-(r/1400f32))) as u8 ;
+            }
+        }
+        self.phase += 5;
+    }
+    
+    pub fn blend(&mut self, other: &Self) {
+        for y in 0..600 {
+            for x in 0..800 {
+                self.brightness[y][x] = ((self.brightness[y][x] as i32 + other.brightness[y][x] as i32) / 2) as u8;
+            }
+        }
+    }
 }
 
 pub fn main() {
@@ -59,7 +87,8 @@ pub fn main() {
 
     let mut canvas = window.into_canvas().build().unwrap();
 
-    let mut buffer = canvas_buffer{brightness: [[0;800];600],phase:0};
+    let mut buffer: Box<canvas_buffer> = Box::new(canvas_buffer{brightness: vec![vec![0;800];600],phase:0});
+    let mut buffer2: Box<canvas_buffer> = Box::new(canvas_buffer{brightness: vec![vec![0;800];600],phase:0});
     
     canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.clear();
@@ -80,7 +109,10 @@ pub fn main() {
             }
         }
         // buffer.sine_wave();
-        buffer.circle(Point::new(400, 300), 80f32);
+        // buffer.circle(Point::new(400, 300), 80f32);
+        buffer.circle2(Point::new(200, 350), 80f32);
+        buffer2.circle2(Point::new(600, 350), 40f32);
+        buffer.blend(&buffer2);
         buffer.draw_on_canvas(&mut canvas);
         // The rest of the game loop goes here...
         
